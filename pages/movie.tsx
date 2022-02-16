b05dcbd1a76ce350620eb6b9d131c8de49cb7061
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-16 22:39:39 +0545

    Develop movie page

--- pages/movie.tsx
+++ pages/movie.tsx
@@ -1,14 +1,88 @@
 import Image from "next/image";
+import Head from "next/head";
 import React from "react";
 import Layout from "../components/Layout";
-
-const Movie = ({ movie }: any) => {
+import styles from "../styles/Movie.module.scss";
+import { RatingStar } from "rating-star";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { AiOutlineClockCircle, AiOutlineLike } from "react-icons/ai";
+import moment from "moment";
+const Movie = ({ movie }: { [key: string]: any }) => {
   console.log(movie);
 
   return (
-    <Layout>
-      <h1>{movie.title}</h1>
-    </Layout>
+    <>
+      <Head>
+        <title>{movie.title} - movie</title>
+      </Head>
+      <Layout>
+        <div className={styles.movie_page}>
+          <div className={styles.background}>
+            <Image
+              src={movie.background_image}
+              layout="fill"
+              className="background_image"
+              alt={movie.title}
+            />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.intro}>
+              <div className={styles.thumbnail_container}>
+                <Image
+                  alt={movie.title}
+                  src={movie.medium_cover_image}
+                  height="400px"
+                  width="300px"
+                  layout="responsive"
+                  className={styles.thumbnail_image}
+                  objectFit="cover"
+                />
+              </div>
+              <div className={styles.intro_content}>
+                <h1>
+                  {movie.title} <span>({movie.year})</span>
+                </h1>
+                <p className={styles.description}>{movie.description_full}</p>
+
+                <p className={styles.genres}>
+                  {movie.genres.map((genre: string, index: number) => {
+                    return <small key={index}>{genre}</small>;
+                  })}
+                </p>
+                <div className={styles.rating}>
+                  {movie.rating}
+                  <RatingStar
+                    id={`${movie.id}`}
+                    noBorder
+                    rating={movie.rating}
+                    maxScore={10}
+                  />
+                </div>
+                <p className={styles.info}>
+                  <span className={styles.download_count}>
+                    {" "}
+                    <MdOutlineFileDownload /> {movie.download_count}
+                  </span>
+                  <span className={styles.runtime}>
+                    {" "}
+                    <AiOutlineClockCircle />
+                    <span>{movie.runtime} minutes</span>
+                  </span>
+                  <span className={styles.runtime}>
+                    {" "}
+                    <AiOutlineLike />
+                    <span>{movie.like_count}</span>
+                  </span>
+                </p>
+                <p>
+                  Uploaded on {moment(movie.date_uploaded).format("DD/MM/YYYY")}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 };
 
