1d26c79269aa11984b704b63fd130c552c1bf8b4
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-16 20:46:30 +0545

    Add crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,11 @@ const Document = () => {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Montserrat:wght@200;300;400;500;600;700;800&family=Poppins:wght@200;300;400;500;600;700&display=swap"
           rel="stylesheet"
